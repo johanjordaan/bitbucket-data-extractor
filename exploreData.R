5ad711d40d16a7034766165d4ff7a6cf776a9551
Johan Jordaan <djjordaan@gmail.com>
2016-02-07 19:06:49 +0200

    Lots of work on grouping users

--- exploreData.R
+++ exploreData.R
@@ -1,37 +1,98 @@
+library(stringdist)
+
 library(ggplot2)
 library(data.table)
 
-commits <- read.csv("commits.csv")
-commits$user <- gsub(" <.*$","",commits$author)
-users <- unique(commits$user)
+extractFirstname <- function(tokens) {
+   retVal <- tokens[1]
+   tolower(retVal)
+}
 
-extract <- function(tokens, i) {
-   if(length(tokens) == 1) {
-      tokens = unlist(strsplit(tokens[1], "(?<=[A-Z])(?=[A-Z])", perl = TRUE))
-      tokens[i]
-   } else {
-      if(i==2) {
-         paste(tokens[i:length(tokens)], collapse = ' ')
-      } else {
-         tokens[1]
-      }
+extractSurname <- function(tokens) {
+   l <- length(tokens)
+   retVal <- NA
+
+   if(l == 1) {
+      retVal <- ""
+   } else if (l == 2) {
+      retVal <- tokens[2]
+   } else if(l > 2) {
+      retVal <- paste0(tokens[2:l-1][grepl("^[a-z]",tokens[2:l-1])],tokens[l])
+      retVal <- gsub("([a-z])([A-Z])", "\\1 \\2",retVal)
+   }
+
+   tolower(retVal)
+}
+
+extractNames <- function(tokens) {
+   firstname <- extractFirstname(tokens)
+   surname <- extractSurname(tokens)
+
+   names <- tolower(paste(tokens,collapse=" "))
+   names <- gsub(firstname,"",names)
+   names <- gsub(surname,"",names)
+   names <- gsub("^[ ]+|[ ]+$","",names)
+}
+
+extractInitial <- function(str) {
+   retVal <- substr(str,1,1)
+   tolower(retVal)
+}
+
+
+commits <- read.csv("commits.csv")
+commits$user <- commits$author
+commits$user <- gsub(" <.*$","",commits$user)    # Kill the email part
+commits$user <- gsub("[_@-]","",commits$user)    # Kill special chacracters
+commits$user <- gsub("[ ]+"," ",commits$user)     # Replace multiple ws with a single space
+
+users <- unique(commits$user)
+tokens <- strsplit(gsub("([A-Z])", " \\1",users)," ")
+tokens <- lapply(tokens,function(t) t[t!=""])
+
+firstname <- sapply(tokens, extractFirstname)
+surname <- sapply(tokens, extractSurname)
+names <- sapply(tokens,extractNames)
+firstnameInitial <- extractInitial(firstname)
+surnameInitial <- extractInitial(surname)
+
+createVariant <- function(a,b) {
+   retVal = gsub("[ ]","",paste0(a,b,colapse=""))
+}
+
+variant_1 <- createVariant(firstname,surname)
+variant_2 <- createVariant(surname,firstname)
+variant_3 <- createVariant(firstnameInitial,surname)
+variant_4 <- createVariant(firstname,surnameInitial)
+
+DT <- data.table(  variant_1=variant_1
+                  ,variant_2=variant_2
+                  ,variant_3=variant_3
+                  ,variant_4=variant_4
+                )
+SIM <- data.table()
+
+s = nrow(DT)
+for(i in 1:(s-1)) {
+   for(j in (i+1):s) {
+      mins <- apply(m,2,min)  # by col
+      m <- stringdistmatrix(as.character(DT[i,]),as.character(DT[j,]),method="lv")
+      print("--------")
+      print(i)
+      print(j)
+      print(mean(mins))
    }
 }
 
-getname <- function(str) {
-   lapply(strsplit(str,"[ _]"), function(x) extract(x,1) )
-}
-
-getsurname <- function(str) {
-   lapply(strsplit(str,"[ _]"), function(x) extract(x,2) )
-}
-
-DT <- data.table(fullname=users)
-DT <- DT[,.(fullname,name=getname(fullname),surname=getsurname(fullname)),]
-
-
-
-
+#grid <- expand.grid(i=1:nrow(DT)-1,j=2:nrow(DT))
+#for(p in 1:nrow(grid)) {
+#   i <- grid$i[p]
+#   j <- grid$j[p]
+#
+#   m <- stringdistmatrix(as.character(DT[i,]),as.character(DT[j,]),method="lv")
+#   mins <- apply(m,2,min)  # by col
+#   #print(mean(mins))
+#}
 
 
 
